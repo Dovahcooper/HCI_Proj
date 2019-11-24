ec2e8de93fc1dbb15c283f82294843a85a252d5b
Dovahcooper <matthew.holt@ontariotechu.net>
2019-11-24 15:34:07 -0500

    Arduino and Controls
    all controls are implemented now. Just needs animations/textures implemented

---
+++ UnityProj/Human-Computer-Interaction/Assets/Plugins/XInput.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+namespace XBOX
+{
+    public struct Stick
+    {
+        public float xAxis;
+        public float yAxis;
+    }
+
+    public struct Triggers
+    {
+        public float lTrig;
+        public float rTrig;
+    }
+
+    public enum Buttons
+    {
+        A = 0x5800,
+        B = 0x5801,
+        X = 0x5802,
+        Y = 0x5803,
+        RB = 0x5804,
+        LB = 0x5805,
+        LTrig = 0x5806,
+        RTrig = 0x5807,
+
+        DPadUp = 0x5810,
+        DPadDown = 0x5811,
+        DPadLeft = 0x5812,
+        DPadRight = 0x5813,
+        Start = 0x5814,
+        Select = 0x5815,
+        L3 = 0x5816,
+        R3 = 0x5817,
+
+        LS_Up = 0x5820,
+        LS_Down = 0x5821,
+        LS_Right = 0x5822,
+        LS_Left = 0x5823,
+        LS_Up_Left = 0x5824,
+        LS_Up_Right = 0x5825,
+        LS_Down_Right = 0x5826,
+        LS_Down_Left = 0x5827,
+    };
+
+
+    public class XInput : MonoBehaviour
+    {
+        const string dllName = "XInput1_4 Wrapper";
+
+        [DllImport(dllName)]
+        private static extern void initDLL();
+
+        [DllImport(dllName)]
+        public static extern bool GetConnected(int _index = 0);
+
+        [DllImport(dllName)]
+        private static extern bool DownloadPackets(int num_controllers = 1);
+
+        [DllImport(dllName)]
+        private static extern void UpdateController(int _index = 0);
+
+        [DllImport(dllName)]
+        public static extern bool GetKeyPressed(int _index, int _button);
+
+        [DllImport(dllName)]
+        public static extern bool GetKeyReleased(int _index, int _button);
+
+        [DllImport(dllName)]
+        //public static extern Stick GetLeftStick(int _index = 0);
+        public static extern float GetLeftX(int index = 0);
+
+        [DllImport(dllName)]
+        public static extern float GetLeftY(int index = 0);
+
+        [DllImport(dllName)]
+        public static extern float GetRightX(int index = 0);
+
+        [DllImport(dllName)]
+        public static extern float GetRightY(int index = 0);
+
+        [DllImport(dllName)]
+        public static extern Stick GetRightStick(int _index = 0);
+
+        [DllImport(dllName)]
+        //public static extern Triggers GetTriggers(int _index = 0);
+        public static extern float GetLeftTrigger(int index = 0);
+
+        [DllImport(dllName)]
+        //public static extern Triggers GetTriggers(int _index = 0);
+        public static extern float GetRightTrigger(int index = 0);
+
+        [DllImport(dllName)]
+        public static extern bool SetVibration(int _index = 0, float l_motor = 0.0f, float r_motor = 0.0f);
+
+        [DllImport(dllName)]
+        private static extern void cleanDLL();
+
+        public static Buttons[] arr = new Buttons[]{
+            Buttons.A,
+            Buttons.B,
+            Buttons.X,
+            Buttons.Y,
+            Buttons.RB,
+            Buttons.LB,
+            Buttons.RTrig,
+            Buttons.LTrig,
+            Buttons.DPadUp,
+            Buttons.DPadDown,
+            Buttons.DPadLeft,
+            Buttons.DPadRight,
+            Buttons.Start,
+            Buttons.Select,
+            Buttons.R3,
+            Buttons.L3,
+        };
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            initDLL();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            DownloadPackets();
+            UpdateController();
+        }
+
+        private void OnDestroy()
+        {
+            cleanDLL();
+        }
+
+        public static bool AnyButton()
+        {
+            for(int i = 0; i < 16; i++)
+            {
+                if(GetKeyPressed(0, (int)arr[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static void AnyButton(ref Buttons button)
+        {
+            for(int i = 0; i < 16; i++)
+            {
+                if(GetKeyPressed(0, (int)arr[i]))
+                {
+                    button = arr[i];
+                }
+            }
+        }
+    }
+}
---
+++ UnityProj/Human-Computer-Interaction/Assets/Sprites/Player.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using XBOX;
+using Ardunity;
+
+public class Player : MonoBehaviour
+{
+    float speed = 25f;
+    public float firstJump = 15f;
+    private float secondJump = 10f;
+
+    public Vector2 move_input;
+    public Rigidbody2D playerBody;
+
+    private bool jumping;
+    private bool canJump;
+
+    public ArdunityApp controller;
+    public AnalogInput controllerX;
+    public DigitalInput controllerButton;
+
+    private bool contBut;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody.GetComponent<Rigidbody2D>();
+
+        controller = GameObject.FindGameObjectWithTag("Ardunity").GetComponent<ArdunityApp>();
+        controllerX = GameObject.FindGameObjectWithTag("PlayerX").GetComponent<AnalogInput>();
+        controllerButton = GameObject.FindGameObjectWithTag("PlayerButton").GetComponent<DigitalInput>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (controller.connected)
+        {
+            ArduinoInputs();
+        }
+        else if (XInput.GetConnected())
+        {
+            ControllerInputs();
+        }
+        else
+        {
+            KeyboardInputs();
+        }
+    }
+    
+    void KeyboardInputs()
+    {
+        KeyboardMovement(move_input);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerJump();
+        }
+    }
+
+    void ControllerInputs()
+    {
+        ControllerMovement(move_input);
+        if(XInput.GetKeyPressed(0, (int)Buttons.A))
+        {
+            PlayerJump();
+        }
+    }
+
+    void ArduinoInputs()
+    {
+        ArduinoMovement(move_input);
+        
+        if(contBut && !controllerButton.Value)
+        {
+            if (canJump)
+                PlayerJump();
+            else
+                PlayerDash();
+        }
+    }
+
+    void KeyboardMovement(Vector2 moveInput)
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = 0f;
+
+        MovePlayer(moveInput);
+    }
+
+    void ControllerMovement(Vector2 moveInput)
+    {
+        moveInput.x = XInput.GetLeftX();
+        moveInput.y = 0f;
+
+        MovePlayer(moveInput);
+    }
+
+    void ArduinoMovement(Vector2 moveInput)
+    {
+        if(controllerX.Value < 0.48f || controllerX.Value > 0.52f)
+        {
+            moveInput.x = (controllerX.Value - 0.5f) * -2f;
+        }
+        moveInput.y = 0f;
+
+        MovePlayer(moveInput);
+    }
+
+    void MovePlayer(Vector2 moveInput)
+    {
+        playerBody.velocity = moveInput * speed * Time.fixedDeltaTime;
+
+        if(moveInput.x < 0f)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+
+    void PlayerJump()
+    {
+        if (!jumping)
+        {
+            playerBody.AddForce(firstJump * Vector2.up, ForceMode2D.Impulse);
+            jumping = true;
+        }
+        else
+        {
+            playerBody.AddForce(secondJump * Vector2.up, ForceMode2D.Impulse);
+        }
+
+    }
+
+    void PlayerDash()
+    {
+        if (jumping)
+        {
+            Vector2 tempVec = new Vector2(40f * move_input.x, 0f);
+
+            playerBody.velocity = tempVec;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.rigidbody.position.y < playerBody.position.y)
+        {
+            canJump = true;
+            jumping = false;
+        }
+    }
+}
